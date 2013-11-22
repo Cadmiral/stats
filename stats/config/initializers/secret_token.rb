a660e938c7a82a400002b153af007f0a0656c288
administrator <ca.nguyen@framehawk.com>
2013-11-22 15:35:51 -0800

    test

---
+++ stats/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stats::Application.config.secret_key_base = '53ccd018e5b013ceeb59595b94706963feec559fe44bbf2bc94319c4bb5996284ef44b1ca38e2cf553a8e018834811d516484e369e9625ec7497c5dd50c02869'
