fdbb993f6f3349599af051c3052395577a9795ef
Steven Funasaki <steven.funasaki@framehawk.com>
2013-11-28 23:17:07 -1000

    Adding Injury Manager and Player Manager
    Signed-off-by: Steven Funasaki <steven.funasaki@framehawk.com>

---
+++ stats/script/main.rb
@@ -0,0 +1,15 @@
+
+require_relative 'InjuryManager'
+
+
+begin
+	# Populate Player Object
+	#playerList = NBAPlayerManager.new
+
+	#
+
+
+	# Check Injuries
+	injuryList = InjuryManager.new
+	injuryList.runTest
+end
